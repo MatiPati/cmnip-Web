93c317a1e37a78ea4f38758470734176aab6f02a
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-02-26 11:09:04 +0100

    Home page graph added!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-3">
                 <h1 class="m-0">
-                    This is HOME
+                    CMNiN
                 </h1>
                 <hr class="mb-2">
                 <p>
@@ -12,16 +12,9 @@
             </div>
             <div class="col-lg-9">
                 <highcharts :options="chartOptions" style="width: 100%; height: 500px"></highcharts>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="d-flex mt-3">
-                            <input class="form-control" v-model="newName">
-                            <button class="btn btn-outline-dark" @click="addNameToGraph()">Add</button>
-                        </div>
-                    </div>
+                <div class="mt-3 text-right">
+                    <button class="btn btn-outline-dark">Check name on graph</button>
                 </div>
-
-
 
             </div>
         </div>
@@ -45,7 +38,7 @@
                 visibleNames: [],
                 chartOptions: {
                     title: {
-                        text: 'Name uses frequency graph'
+                        text: 'Name uses frequency'
                     },
                     plotOptions: {
                         series: {
@@ -55,39 +48,44 @@
                             pointStart: 2000
                         }
                     },
+                    yAxis: {
+                        title: {
+                            text: 'Names given'
+                        }
+                    },
+                    xAxis: {
+                        title: {
+                            text: 'Year'
+                        }
+                    },
+                    credits: {
+                        enabled: false
+                    },
                     series: []
                 }
             }
         },
         created() {
-            this.getData('Antoni');
+            this.getData('Karolina');
             setTimeout(() => {
-                this.getData('Jakub');
+                this.getData('Patryk');
             }, 2000);
             setTimeout(() => {
-                this.getData('Jan');
+                this.getData('Gabriela');
             }, 4000);
             setTimeout(() => {
-                this.getData('Szymon');
+                this.getData('Mateusz');
             }, 6000);
+            setTimeout(() => {
+                this.getData('Paweł');
+            }, 8000);
         },
 
         methods: {
-            addNameToGraph () {
-                this.getData(this.newName.charAt(0).toUpperCase() + this.newName.slice(1));
-                this.newName = '';
-            },
             getData(name) {
                 fetch('https://mvtthew.pl:11290/name/' + name, {
                     method: 'GET'
                 }).then(res => res.json()).then(data => {
-
-                    // if (this.nameIndex === 0) {
-                    //     const years = Object.keys(data.years);
-                    //     years.forEach(year => {
-                    //         this.option.xAxis.data.push(year);
-                    //     });
-                    // }
 
                     this.chartOptions.series.push(
                         {
